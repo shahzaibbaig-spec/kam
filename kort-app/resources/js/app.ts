ae843ba122651b81b6b9bf9d9b7c605ecb045837
Muhammad Shahzaib Baig <shahzaib.baig@gmail.com>
2026-04-06 14:05:06 +0500

    new fix

--- kort-app/resources/js/app.ts
+++ kort-app/resources/js/app.ts
@@ -2,19 +2,23 @@ import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pages = import.meta.glob<DefineComponent>('./Pages/**/*.vue', { eager: true });
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
-        ),
+    resolve: (name) => {
+        const page = pages[`./Pages/${name}.vue`];
+
+        if (!page) {
+            throw new Error(`Inertia page not found: ${name}`);
+        }
+
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
